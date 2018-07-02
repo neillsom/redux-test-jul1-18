4e73ca1c749a96e65afef4ebd3c1455bbe9a3d2a
neillsom <neillsom@mail.com>
2018-07-01 22:54:24 -0700

    Provider added, connect implemented, mapstatetoprops connected

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import {incrementCounter, decrementCounter} from './actions';
 
 class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Count is: {this.props.count}</h1>
+        <button onClick={() => this.props.dispatch(incrementCounter(1))}>Increment</button>
+        <button onClick={() => this.props.dispatch(decrementCounter(1))}>Decrement</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state);
+
+  return {
+    count: state.counterReducer.count
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import { incrementCounter, decrementCounter } from './actions';
-
-console.log(store.getState());
-store.dispatch(incrementCounter(10));
-console.log(store.getState());
-store.dispatch(decrementCounter(5));
-console.log(store.getState());
+import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
